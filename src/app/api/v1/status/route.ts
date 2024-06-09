73cd6cf7e0c2f04200777d81afd3d1cb446b490e
Thielson Alemendra <thielson12@gmail.com>
2024-06-09 19:14:00 +0000

    feat: config module `database.ts` and `.env` file

--- src/app/api/v1/status/route.ts
+++ src/app/api/v1/status/route.ts
@@ -1,6 +1,9 @@
+import { db } from "@/infra/database";
 import { NextRequest, NextResponse } from "next/server";
 
 export const dynamic = "force-dynamic";
-export async function GET(request: NextRequest) {
+export async function GET(_: NextRequest) {
+  const result = await db.query("SELECT 1 + 1 as sum;");
+  console.log(result.rows);
   return NextResponse.json({ message: "Primeiro endpoint" }, { status: 200 });
 }
