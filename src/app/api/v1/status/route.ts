d2d6240c151c03f5b9b708b54d71545bb6c8d94f
Thielson Alemendra <thielson12@gmail.com>
2024-06-09 21:38:21 +0000

    feat: finish route status

--- src/app/api/v1/status/route.ts
+++ src/app/api/v1/status/route.ts
@@ -3,7 +3,31 @@ import { NextRequest, NextResponse } from "next/server";
 
 export const dynamic = "force-dynamic";
 export async function GET(_: NextRequest) {
-  const result = await db.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  return NextResponse.json({ message: "Primeiro endpoint" }, { status: 200 });
+  const updatedAt = new Date().toISOString();
+  const databaseVersion = (await db.query("SHOW server_version;")).rows[0]
+    .server_version;
+  const databaseMaxConnections = (await db.query("SHOW max_connections;"))
+    .rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenedConnections = (
+    await db.query({
+      text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+      values: [databaseName],
+    })
+  ).rows[0].count;
+
+  return NextResponse.json(
+    {
+      updated_at: updatedAt,
+      dependencies: {
+        database: {
+          version: databaseVersion,
+          max_connections: parseInt(databaseMaxConnections),
+          openend_connections: databaseOpenedConnections,
+        },
+      },
+    },
+    { status: 200 },
+  );
 }
--- src/tests/integration/app/api/v1/status/get.test.ts
+++ src/tests/integration/app/api/v1/status/get.test.ts
@@ -1,6 +1,14 @@
 describe("GET to /api/v1/status", () => {
   it("should be able to return 200", async () => {
-    const responso = await fetch("http://localhost:3000/api/v1/status");
-    expect(responso.status).toBe(200);
+    const response = await fetch("http://localhost:3000/api/v1/status");
+    const data = await response.json();
+    expect(response.status).toBe(200);
+
+    const parsedUpdatedAt = new Date(data.updated_at).toISOString();
+    expect(data.updated_at).toEqual(parsedUpdatedAt);
+
+    expect(data.dependencies.database.version).toBe("16.0");
+    expect(data.dependencies.database.max_connections).toBe(100);
+    expect(data.dependencies.database.openend_connections).toBe(1);
   });
 });
