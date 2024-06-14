c595f1e769f5e3ba6dbd38b7ac8a20e52c3c7396
Thielson Alemendra <thielson12@gmail.com>
2024-06-14 20:08:09 +0000

    fix: not found migrations files on prod

---
+++ next.config.mjs
@@ -0,0 +1,6 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: { nftTracing: true },
+};
+
+export default nextConfig;
--- src/app/api/v1/migrations/route.ts
+++ src/app/api/v1/migrations/route.ts
@@ -2,6 +2,7 @@ import migrationRunner, { RunnerOption } from "node-pg-migrate";
 import { NextRequest, NextResponse } from "next/server";
 import { join } from "path";
 import { db } from "@/infra/database";
+import { RunMigration } from "node-pg-migrate/dist/migration";
 export const dynamic = "force-dynamic";
 
 const defaultMigrationOptions: Omit<RunnerOption, "databaseUrl" | "dbClient"> =
@@ -15,22 +16,37 @@ const defaultMigrationOptions: Omit<RunnerOption, "databaseUrl" | "dbClient"> =
 
 export async function GET(_: NextRequest) {
   const dbClient = await db.getNewClient();
-  const pedingMigrations = await migrationRunner({
-    ...defaultMigrationOptions,
-    dbClient,
-  });
-  await dbClient.end();
+  let pedingMigrations: RunMigration[];
+  try {
+    pedingMigrations = await migrationRunner({
+      ...defaultMigrationOptions,
+      dbClient,
+    });
+  } catch (e) {
+    console.log(e);
+    throw e;
+  } finally {
+    if (dbClient) await dbClient.end();
+  }
+
   return NextResponse.json(pedingMigrations, { status: 200 });
 }
 
 export async function POST(_: NextRequest) {
   const dbClient = await db.getNewClient();
-  const migratedMigrations = await migrationRunner({
-    ...defaultMigrationOptions,
-    dryRun: false,
-    dbClient,
-  });
-  await dbClient.end();
+  let migratedMigrations: RunMigration[];
+  try {
+    migratedMigrations = await migrationRunner({
+      ...defaultMigrationOptions,
+      dryRun: false,
+      dbClient,
+    });
+  } catch (e) {
+    console.log(e);
+    throw e;
+  } finally {
+    await dbClient.end();
+  }
   return NextResponse.json(migratedMigrations, {
     status: migratedMigrations.length === 0 ? 200 : 201,
   });
