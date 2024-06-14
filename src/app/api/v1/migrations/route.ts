0c59a47be403cd7118e641fc48675bf507f48b16
Thielson Alemendra <thielson12@gmail.com>
2024-06-14 19:34:21 +0000

    refactor: database module e migration route

--- src/app/api/v1/migrations/route.ts
+++ src/app/api/v1/migrations/route.ts
@@ -1,29 +1,36 @@
 import migrationRunner, { RunnerOption } from "node-pg-migrate";
 import { NextRequest, NextResponse } from "next/server";
 import { join } from "path";
+import { db } from "@/infra/database";
 export const dynamic = "force-dynamic";
 
-const defaultMigrationOptions: RunnerOption = {
-  dir: join("src", "infra", "migrations"),
-  databaseUrl: process.env.DATABASE_URL,
-  dryRun: true,
-  direction: "up",
-  verbose: true,
-  migrationsTable: "pgmigrations",
-};
+const defaultMigrationOptions: Omit<RunnerOption, "databaseUrl" | "dbClient"> =
+  {
+    dir: join("src", "infra", "migrations"),
+    dryRun: true,
+    direction: "up",
+    verbose: true,
+    migrationsTable: "pgmigrations",
+  };
 
 export async function GET(_: NextRequest) {
+  const dbClient = await db.getNewClient();
   const pedingMigrations = await migrationRunner({
     ...defaultMigrationOptions,
+    dbClient,
   });
+  await dbClient.end();
   return NextResponse.json(pedingMigrations, { status: 200 });
 }
 
 export async function POST(_: NextRequest) {
+  const dbClient = await db.getNewClient();
   const migratedMigrations = await migrationRunner({
     ...defaultMigrationOptions,
     dryRun: false,
+    dbClient,
   });
+  await dbClient.end();
   return NextResponse.json(migratedMigrations, {
     status: migratedMigrations.length === 0 ? 200 : 201,
   });
--- src/infra/database.ts
+++ src/infra/database.ts
@@ -1,32 +1,5 @@
 import { Client, QueryConfig } from "pg";
 
-async function query(queryConfig: string | QueryConfig, values?: any[]) {
-  const client = new Client({
-    host: process.env.POSTGRES_HOST,
-    port: Number(process.env.POSTGRES_PORT),
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
-    ssl: getSSLValues(),
-  });
-  try {
-    await client.connect();
-    const result = await client.query(queryConfig, values);
-    return result;
-  } catch (error) {
-    console.error("Error executing query:", error);
-    throw error;
-  } finally {
-    await client.end();
-  }
-}
-
-export const db: {
-  query: typeof query;
-} = {
-  query,
-};
-
 function getSSLValues() {
   if (process.env.POSTGRES_CA) {
     return {
@@ -36,3 +9,40 @@ function getSSLValues() {
 
   return process.env.NODE_ENV === "production" ? true : false;
 }
+
+async function getNewClient() {
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: Number(process.env.POSTGRES_PORT),
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+    ssl: getSSLValues(),
+  });
+  await client.connect();
+  return client;
+}
+
+async function query(queryConfig: string | QueryConfig, values?: any[]) {
+  let client: Client;
+  try {
+    client = await getNewClient();
+    const result = await client.query(queryConfig, values);
+    return result;
+  } catch (error) {
+    console.error("Error executing query:", error);
+    throw error;
+  } finally {
+    if (client) await client.end();
+  }
+}
+
+type dbReturn = {
+  query: typeof query;
+  getNewClient: typeof getNewClient;
+};
+
+export const db: dbReturn = {
+  query,
+  getNewClient,
+};
