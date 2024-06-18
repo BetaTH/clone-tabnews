33c17fe916b2c8c52f71245b83f0b2160e9a4fc6
Thielson Alemendra <thielson12@gmail.com>
2024-06-18 04:18:09 +0000

    feat: make `npm test` more fast

--- src/tests/orchestrator.ts
+++ src/tests/orchestrator.ts
@@ -1,13 +1,16 @@
 import retry from "async-retry";
 async function waitForAllServices() {
   async function waitForWebServer() {
-    async function fetchStatusPage() {
-      const response = await fetch("http://localhost:3000/api/v1/status");
-      const data = await response.json();
-      if (response.status !== 200) throw new Error();
-    }
+    const fetchStatusPage: retry.RetryFunction<void> = async (_, attempt) => {
+      const reponse = await fetch("http://localhost:3000/api/v1/status");
+      if (!reponse.ok) {
+        throw Error();
+      }
+    };
+
     return retry(fetchStatusPage, {
       retries: 100,
+      maxTimeout: 1000,
     });
   }
 
