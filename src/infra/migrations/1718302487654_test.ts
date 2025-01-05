c7edf4c5a0c3eda81417d6d140f62c6b2868c3bb
Thielson Alemendra <thielson12@gmail.com>
2025-01-05 18:04:45 +0000

    fix:(migrations): disable eslint rule

--- src/infra/migrations/1718302487654_test.ts
+++ src/infra/migrations/1718302487654_test.ts
@@ -2,6 +2,9 @@ import { ColumnDefinitions, MigrationBuilder } from 'node-pg-migrate';
 
 export const shorthands: ColumnDefinitions | undefined = undefined;
 
+// eslint-disable-next-line no-unused-vars
 export async function up(pgm: MigrationBuilder): Promise<void> {}
 
+
+// eslint-disable-next-line no-unused-vars
 export async function down(pgm: MigrationBuilder): Promise<void> {}
